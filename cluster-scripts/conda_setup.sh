bfd50cce9974104e5ad0756d6d5d973981e34676
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-12-09 11:15:47 -0500

    cluster conda setup

---
+++ cluster-scripts/conda_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=deepspeed-conda
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=256G
+#SBATCH --time=04:00:00
+#SBATCH --output=deepspeed-conda-slurm-%j.output
+#SBATCH --error=deepspeed-conda-slurm-%j.err
+
+module load cuda/11.8.0
+module load anaconda3
+
+conda env create -n deepspeed -f ../environment.yml --force
